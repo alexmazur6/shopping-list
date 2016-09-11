1d2015663740409722312f1d0cd3fa2be88441ed
detroitcoder9 <alexmazur6@gmail.com>
2016-09-11 19:49:21 -0400

    Formating on shopping list append

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ $(document).ready(function() {
 	$("#js-shopping-list-form").children("button").click(function(event) {
 		event.preventDefault();
 		var newListItem = $("#shopping-list-entry").val();
-		$(".shopping-list").append("<li>" + 
-			"<span class='shopping-item'>" + newListItem + "</span>" +
-			"<div class=shopping-item-controls'>" +
-			"<button class='shopping-item-toggle'>" +
-            "<span class='button-label'>check</span>" +
-          	"</button>" +
-         	"<button class='shopping-item-delete'>" +
-            "<span class='button-label'>delete</span>" +
-          	"</button>" +
-        	"</div>" +
+		$(".shopping-list").append(
+			"<li>" + 
+				"<span class='shopping-item'>" + newListItem + "</span>" +
+				"<div class=shopping-item-controls'>" +
+					"<button class='shopping-item-toggle'>" +
+            			"<span class='button-label'>check</span>" +
+          			"</button>" +
+         			"<button class='shopping-item-delete'>" +
+            			"<span class='button-label'>delete</span>" +
+          			"</button>" +
+        		"</div>" +
       		"</li>"
       	);
-      	$("#shopping-list-entry").val("");
+      	$("#shopping-list-entry").val("")
 	});
 
 	$(".shopping-list").on('click', '.shopping-item-toggle', function() {
